f395ef4dac89702575bb3a27825230af97e74f1a
[Bruno] <[bruno.micalli@uol.com.br]>
2021-04-27 14:02:58 -0300

    Criando os components

--- blogPessoal/blogPessoa2/src/app/app.module.ts
+++ blogPessoal/blogPessoa2/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
